2c7f41bdb0d634fcd28986449b88d63f1c6a3097
MakeItLonger <zavadiuk@gmail.com>
2024-04-29 16:26:40 +0300

    use param and body decorators, throw internal nest error object by condition

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import CreateDto from './dto/create.dto';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('get/:id')
+  getHello(@Param('id', ParseIntPipe) id: number) {
+    if (id < 1) {
+      throw new BadRequestException('id must be greater than 0');
+    }
+    return id;
+  }
+
+  @Post('create')
+  create(@Body() dto: CreateDto) {
+    return dto;
   }
 }
